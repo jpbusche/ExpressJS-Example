bc75b42e26e4a094cc0a9c678c35eaff1b9d12b0
João Paulo Busche <joaopaulo.busche@gmail.com>
2018-07-17 15:57:54 -0300

    Adding passport to project

--- app.js
+++ app.js
@@ -4,8 +4,40 @@ const session = require('express-session');
 const bcrypt = require('bcrypt-nodejs');
 const axios = require('axios');
 const path = require('path');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy
 
 const app = express();
+
+passport.use(new LocalStrategy(
+	{usernameField: 'username'},
+	function(username, password, done) {
+		axios.get('http://localhost:5000/users?username=' + username).then(function(res){
+			const user = res.data[0];
+			if(!user) {
+				return done(null, false, {message: 'Invalid credentials.\n'});
+			}
+			if(!bcrypt.compareSync(password, user.password)) {
+				return done(null, false, {message: 'Invalid credentials.\n'});
+			}
+			return done(null, user);
+		}).catch(function(err){
+			console.log(err);
+			done(err);
+		});
+	}
+));
+passport.serializeUser(function(user, done){
+	done(null, user.id);
+});
+passport.deserializeUser(function(id, done){
+	axios.get('http://localhost:5000/users/' + id).then(function(res){
+		done(null, res.data);
+	}).catch(function(err){
+		console.log(err);
+		done(err, false);
+	});
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -40,7 +72,8 @@ app.post('/signup', function(req, res) {
 });
 
 app.get('/some_page', function(req, res) {
-	res.send("SOME PAGE for you: " + req.session.user.usename)
+	var username = req.session.user.username;
+	res.send("SOME PAGE for you: " + username);
 });
 
 app.listen(3000);
